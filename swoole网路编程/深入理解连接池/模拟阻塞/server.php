c659d821b64b3cefa495cd1d252112fd9781369c
yaoxs <980886119@qq.com>
2020-04-15 11:31:21 +0800

    模拟数据阻塞

--- swoole网路编程/深入理解连接池/模拟阻塞/server.php
+++ swoole网路编程/深入理解连接池/模拟阻塞/server.php
@@ -8,6 +8,13 @@
 //RAW: inuse 0
 //FRAG: inuse 0 memory 0
 //当客户端发送1000次的时候 执行cat /proc/net/sockstat，再次查看套接字缓冲区使用量(mem)
+//[root@iZ2zebn3hsgwu61wulgeqhZ 模拟阻塞]# cat /proc/net/sockstat
+//sockets: used 127
+//TCP: inuse 15 orphan 1 tw 1 alloc 15 mem 277
+//UDP: inuse 2 mem 0
+//UDPLITE: inuse 0
+//RAW: inuse 0
+//FRAG: inuse 0 memory 0
 $server = new Swoole\Server("127.0.0.1", 6666, SWOOLE_BASE);
 $server->set([
     "worker_num" => 1
@@ -15,7 +22,7 @@ $server->set([
 
 // 监听连接进入事件
 $server->on('connect', function ($server, $fd){
-    echo "进入{$fd}的监听\n";
+    echo "进入监听\n";
 });
 
 // 监听数据的接收事件
