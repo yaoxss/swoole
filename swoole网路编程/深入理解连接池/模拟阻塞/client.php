13239533248944c427a1f3dd44d6f2c910ed7d39
yaoxs <980886119@qq.com>
2020-04-15 11:06:55 +0800

    模拟数据阻塞

---
+++ swoole网路编程/深入理解连接池/模拟阻塞/client.php
@@ -0,0 +1,13 @@
+<?php
+
+$socket = new CO\Socket(AF_INET, SOCK_STREAM, 0);
+
+go(function () use ($socket){
+    $socket->connect('192.168.2.194', 6666);
+
+    $i = 0;
+    while($i++ < 1000){
+        // str_repeat() 函数把字符串重复指定的次数。
+        var_dump($socket->send(str_repeat("1",1024)));
+    }
+});
---
+++ swoole网路编程/深入理解连接池/模拟阻塞/server.php
@@ -0,0 +1,34 @@
+<?php
+//运行客户端时先使用cat /proc/net/sockstat 查看套接字缓冲区使用量(mem)
+//[root@iZ2zebn3hsgwu61wulgeqhZ ~]# cat /proc/net/sockstat
+//sockets: used 122
+//TCP: inuse 11 orphan 0 tw 1 alloc 11 mem 1
+//UDP: inuse 3 mem 0
+//UDPLITE: inuse 0
+//RAW: inuse 0
+//FRAG: inuse 0 memory 0
+//当客户端发送1000次的时候 执行cat /proc/net/sockstat，再次查看套接字缓冲区使用量(mem)
+$server = new Swoole\Server("127.0.0.1", 6666, SWOOLE_BASE);
+$server->set([
+    "worker_num" => 1
+]);
+
+// 监听连接进入事件
+$server->on('connect', function ($server, $fd){
+    echo "进入{$fd}的监听\n";
+});
+
+// 监听数据的接收事件
+$server->on('receive', function ($server, $fd, $reactor_id, $data){
+    echo $data;
+    // 模拟阻塞
+    sleep(10000);
+});
+
+// 监听连接关闭事件
+$server->on('close', function ($server, $fd){
+    var_dump("close");
+});
+
+// 启动服务
+$server->start();
