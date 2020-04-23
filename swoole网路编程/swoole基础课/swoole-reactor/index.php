47d201df3b501e5a180cebfe0cdf3e091c42fc15
yaoxss <yxsaio2009@163.com>
2020-04-23 19:58:30 +0800

    swoole

--- swoole网路编程/swoole基础课/swoole-reactor/index.php
+++ swoole网路编程/swoole基础课/swoole-reactor/index.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2020\4\23 0023
- * Time: 19:46
- */
+ $http = new swoole_http_server('127.0.0.1', 9501);
+
+ $http->on('request', function ($request, $response) {
+     $redis = new Swoole\Coroutine\Redis();
+     $redis->connect('127.0.0.1', 6379);
+     $result = $redis->get('key');
+     $result = $redis->get($result);
+     $result = $redis->get($result);
+     $result = $redis->get($result);
+     var_dump("终于结束了",$result);
+ });
